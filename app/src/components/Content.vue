2511031377c061279ce4cdeef7c35e15edc52214
Dayakar <hdayakar@gmail.com>
2018-07-15 22:46:45 +0530

    create view

--- app/src/components/Content.vue
+++ app/src/components/Content.vue
@@ -1,9 +1,36 @@
 <template>
-    <h1>Content</h1>
+    <aside class="lg-side">
+        <div class="inbox-head">
+            <h3>{{ currentView.title }}</h3>
+        </div>
+        <keep-alive>
+            <component :is="currentView.tag"></component>
+        </keep-alive>
+    </aside>
 </template>
 
 <script>
+    // import inbox has a local component
+    import Inbox from './Inbox.vue';
+
     export default {
-        
+        data() {
+            return {
+                history: [
+                    {
+                        tag: 'app-inbox',
+                        title: 'Inbox'
+                    }
+                ]
+            }
+        },
+        computed: {
+            currentView() {
+                return this.history[0];
+            }
+        },
+        components: {
+            appInbox: Inbox
+        } 
     }
 </script>
